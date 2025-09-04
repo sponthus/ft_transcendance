22bb9e169d0b1d2b2d4af9e6d114eb5cd4c4151e
zonbo Ndoliam <zonbodiggs@fedora>
2025-09-04 12:57:20 +0200

    add real history in profile page

--- frontend/src/pages/User-Pages/HistoryPage.ts
+++ frontend/src/pages/User-Pages/HistoryPage.ts
@@ -3,38 +3,38 @@ import { getAllGames } from '../../api/game.js';
 import { UserPage } from './UserPage.js';
 
 
-export async function DisplayHistoryPage(Body: HTMLElement) {
-	// Body.textContent = "i'm in the History body";
-	// if(!state.isLoggedIn)
-	// 	Body.textContent = "not connected user";
 
+export async function DisplayHistoryPage(Body: HTMLElement, UserData: any) {
+	// Body.textContent = "i'm in the History body";
 	Body.className = "flex flex-col items-center bg-orange-300  bg-opacity-50 w-full h-[60%] flex overflow-auto";
 	try {
-		// const res = await getAllGames();
-		// if (!res.ok) {
-		// 	Body.textContent = "Error loading games... please retry ";
-		// 	return ;
-		// }
-		// const games = res.games;
-		// if (games.length === 0) {
-		// 	Body.textContent = "there is no games";
-		// }
-		// else {
-		// 	games.map((party: any, i: number) => {
-		// 		const PartyDiv = createDiv("party-div", "flex items-center min-h-[150px] w-[100%] border-2 border-orange-300 hover:border-orange-400 space-x-8");
-
-		// 		createGameId(PartyDiv, i);
-		// 		createPlayers(PartyDiv, i);
-		// 		createBeginAt(PartyDiv, i);
-		// 		createFinishAt(PartyDiv, i);
-		// 		createCreatedAt(PartyDiv, i);
-		// 		createCreatedBy(PartyDiv, i);
-		// 		createScore(PartyDiv, i);;
-		// 		createWinner(PartyDiv, i);
-					
-		// 		append(Body, [PartyDiv]);
-		// 	})
-		// }
+		const res = await getAllGames(UserData.id);
+		if (!res.ok) {
+			Body.textContent = "Error loading games... please retry ";
+			return ;
+		}
+		const games = res.games;
+		if (games.length === 0) {
+			Body.textContent = "there is no games";
+		}
+		else {
+			games.map((party: any, i: number) => {
+				if (party.status == "finished") {
+					const PartyDiv = createDiv("party-div", "flex items-center min-h-[150px] w-[100%] border-2 border-orange-300 hover:border-orange-400 hover:bg-orange-400 hover:bg-opacity-50 space-x-8");
+	
+					createGameId(PartyDiv, i, party);
+					createPlayers(PartyDiv, i, party);
+					createBeginAt(PartyDiv, i, party);
+					createFinishAt(PartyDiv, i, party);
+					createCreatedAt(PartyDiv, i, party);
+					createCreatedBy(PartyDiv, i, UserData.username);
+					createScore(PartyDiv, i, party);;
+					createWinner(PartyDiv, i, party);
+						
+					append(Body, [PartyDiv]);
+				}
+			})
+		}
 
 	}
 	catch (error) {
@@ -43,70 +43,74 @@ export async function DisplayHistoryPage(Body: HTMLElement) {
 }
 
 function createHistoryDiv(id: string) : HTMLElement {
-	const Div = createDiv(id, "h-full w-[11%] border-2 grid grid-rows-4 items-center justify-center space-y-10");
+	const Div = createDiv(id, "h-full w-[11%] border-2 border-orange-300 border-opacity-70 rounded-xl grid grid-rows-4 items-center justify-center");
 	return Div;
 }
 
-function createGameId(PartyDiv: HTMLElement, i: number) {
+function createGameId(PartyDiv: HTMLElement, i: number, Party: any) {
 	const GameIdDivs = createHistoryDiv("Games-id" + i.toString()) as HTMLElement;
-	const GameIdText = createElement('h2', "Games-id" + i.toString(), `Party #${i.toString()} :` , "text-emerald-600 text-center font-boldunderline ");
-	append(GameIdDivs, [GameIdText]);
+	append(GameIdDivs, [(createElement('h2', "Games-id" + i.toString(), `Party :` , "text-emerald-600 text-center font-bold underline order-1") as HTMLElement)
+						, (createElement('h2', "id" + i.toString(), `#${Party.id}` , "text-emerald-600 text-center font-bold row-start-3") as HTMLElement)	]);
 
 	append(PartyDiv, [GameIdDivs]);
 }
 
-function createPlayers(PartyDiv: HTMLElement, i: number) {
+function createPlayers(PartyDiv: HTMLElement, i: number, Party: any) {
 		const PlayersDivs = createHistoryDiv("players" + i.toString()) as 		HTMLElement;
-		const PlayersText = createElement('h2', "players" + i.toString(), `Players :` , "text-emerald-600 text-center font-bold underline ");
-		append(PlayersDivs, [PlayersText]);
+		append(PlayersDivs, [(createElement('h2', "players" + i.toString(), `Players :` , "text-emerald-600 text-center font-bold underline ") as HTMLElement)
+							, (createElement('h2', "players-a" + i.toString(), `${Party.player_a}` , "text-emerald-600 text-center") as HTMLElement)
+							, (createElement('h2', "players-vs" + i.toString(), `VS` , "text-emerald-600 text-center") as HTMLElement)
+							, (createElement('h2', "players-b" + i.toString(), `${Party.player_b}` , "text-emerald-600 text-center") as HTMLElement)]);
 
 		append(PartyDiv, [PlayersDivs]);
 }
 
-function createBeginAt(PartyDiv: HTMLElement, i: number) {
+function createBeginAt(PartyDiv: HTMLElement, i: number, Party: any) {
 	const BeginAtDiv = createHistoryDiv("begin-at" + i.toString()) as HTMLElement;
-	const BeginAtText = createElement('h2', "party-statue" + i.toString(), `Begin At : `, "text-emerald-600 text-center underline font-bold") as HTMLElement;
-	append(BeginAtDiv, [BeginAtText]);
+	append(BeginAtDiv, [(createElement('h2', "party-begin" + i.toString(), `Begin At : `, "text-emerald-600 text-center underline font-bold") as HTMLElement)
+						, (createElement('h2', "begin" + i.toString(), `${Party.began_at}` , "text-emerald-600 text-center row-start-3") as HTMLElement)]);
 
 	append(PartyDiv, [BeginAtDiv]);
 }
 
-function createFinishAt(PartyDiv: HTMLElement, i: number) {
+function createFinishAt(PartyDiv: HTMLElement, i: number, Party: any) {
 	const FinishAtDiv = createHistoryDiv("finish-at" + i.toString()) as HTMLElement;
-	const FinishAtText = createElement('h2', "party-statue" + i.toString(), `Finish At : `, "text-emerald-600 text-center underline font-bold") as HTMLElement;
-	append(FinishAtDiv, [FinishAtText]);
+	append(FinishAtDiv, [(createElement('h2', "party-finish" + i.toString(), `Finish At : `, "text-emerald-600 text-center underline font-bold") as HTMLElement)
+						, (createElement('h2', "finish" + i.toString(), `${Party.finished_at}` , "text-emerald-600 text-center row-start-3") as HTMLElement)]);
 
 	append(PartyDiv, [FinishAtDiv]);
 }
 
-function createCreatedAt(PartyDiv: HTMLElement, i: number) {
+function createCreatedAt(PartyDiv: HTMLElement, i: number, Party: any) {
 	const CreatedAtDivs = createHistoryDiv("created-at" + i.toString()) as HTMLElement;
-	const CreatedAtText = createElement('h2', "party-statue" + i.toString(), `Created At : `, "text-emerald-600 text-center underline font-bold") as HTMLElement;
-	append(CreatedAtDivs, [CreatedAtText]);
+	append(CreatedAtDivs, [(createElement('h2', "party-created" + i.toString(), `Created At : `, "text-emerald-600 text-center underline font-bold") as HTMLElement)
+							, (createElement('h2', "created" + i.toString(), `${Party.created_at}` , "text-emerald-600 text-center row-start-3") as HTMLElement)]);
 
 	append(PartyDiv, [CreatedAtDivs]);
 }
 
-function createCreatedBy(PartyDiv: HTMLElement, i: number) {
+function createCreatedBy(PartyDiv: HTMLElement, i: number, UserName: string) {
 	const CreatedBytDivs = createHistoryDiv("created-by" + i.toString()) as HTMLElement;
-	const CreatedBytText = createElement('h2', "party-statue" + i.toString(), `Created By : `, "text-emerald-600 text-center underline font-bold") as HTMLElement;
-	append(CreatedBytDivs, [CreatedBytText]);
+	append(CreatedBytDivs, [(createElement('h2', "party-statue" + i.toString(), `Created By : `, "text-emerald-600 text-center underline font-bold") as HTMLElement)
+							, (createElement('h2', "created" + i.toString(), `${UserName}` , "text-emerald-600 text-center row-start-3") as HTMLElement)]);
 
 	append(PartyDiv, [CreatedBytDivs]);
 }
 
-function createScore(PartyDiv: HTMLElement, i: number) {
+function createScore(PartyDiv: HTMLElement, i: number, Party: any) {
 	const ScoreDiv = createHistoryDiv("score" + i.toString()) as HTMLElement;
-	const ScoreText = createElement('h2', "party-statue" + i.toString(), `Score: `, "text-emerald-600 text-center underline font-bold") as HTMLElement;
-	append(ScoreDiv, [ScoreText]);
+	append(ScoreDiv, [(createElement('h2', "party-statue" + i.toString(), `Score: `, "text-emerald-600 text-center underline font-bold") as HTMLElement)
+					, (createElement('h2', "players-a" + i.toString(), `${Party.score_a}` , "text-emerald-600 text-center") as HTMLElement)
+					, (createElement('h2', "players-a" + i.toString(), `-` , "text-emerald-600 text-center") as HTMLElement)
+					, (createElement('h2', "players-a" + i.toString(), `${Party.score_b}` , "text-emerald-600 text-center") as HTMLElement)]);
 
 	append(PartyDiv, [ScoreDiv]);
 }
 
-function createWinner(PartyDiv: HTMLElement, i: number) {
+function createWinner(PartyDiv: HTMLElement, i: number, Party: any) {
 	const WinnerDiv = createHistoryDiv("winner" + i.toString()) as HTMLElement;
-	const WinnerText = createElement('h2', "party-statue" + i.toString(), `Winner: `, "text-emerald-600 text-center underline font-bold") as HTMLElement;
-	append(WinnerDiv, [WinnerText]);
+	append(WinnerDiv, [(createElement('h2', "party-statue" + i.toString(), `Winner: `, "text-emerald-600 text-center underline font-bold") as HTMLElement)
+						, (createElement('h2', "players-a" + i.toString(), `${Party.winner}` , "text-emerald-600 text-center row-start-3") as HTMLElement)]);
 
 	append(PartyDiv, [WinnerDiv]);
 }
