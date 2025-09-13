9c416a660c0f2176640eab136e765440f7d95b88
Morgan Bogey <mbogey@z3r3p6.42lyon.fr>
2025-09-13 18:02:02 +0200

    camera changement angle avec fleche omly

--- frontend/src/babylon/pong/displayGame/scene_maker.ts
+++ frontend/src/babylon/pong/displayGame/scene_maker.ts
@@ -27,14 +27,38 @@ export class BabylonSceneBuilder
 	}
 
 	private initializeCamera()
-	{
-		this._camera = new Babylon.ArcRotateCamera("camera", 3.14, 0, 25, Babylon.Vector3.Zero(), this._scene);
-		this._camera.attachControl(this._canvas, true);
-
-		this._camera.upperBetaLimit = 1.3;
-		this._camera.lowerRadiusLimit = 10;
-		this._camera.upperRadiusLimit = 30;
-	}
+    {
+        this._camera = new Babylon.ArcRotateCamera(
+            "camera",
+            Math.PI,
+            0.8,
+            25,
+            Babylon.Vector3.Zero(),
+            this._scene
+        );
+        window.addEventListener("keydown", (evt) => {
+            switch (evt.key) {
+                case "ArrowUp":
+                    this._camera.setPosition(new Babylon.Vector3(0, 20, 0));
+                    this._camera.alpha = 3.14;          // angle horizontal
+                    //this._camera.beta = 0.5;         // angle vertical (vue du dessus)
+                    this._camera.setTarget(Babylon.Vector3.Zero());
+                    break;
+                case "ArrowDown":
+                    this._camera.setPosition(new Babylon.Vector3(-20, 10, 0));
+                    this._camera.setTarget(Babylon.Vector3.Zero());
+                    break;
+                case "ArrowLeft":
+                    this._camera.setPosition(new Babylon.Vector3(0, 10, 20));
+                    this._camera.setTarget(Babylon.Vector3.Zero());
+                    break;
+                case "ArrowRight":
+                    this._camera.setPosition(new Babylon.Vector3(0, 10, -20));
+                    this._camera.setTarget(Babylon.Vector3.Zero());
+                    break;
+            }
+        });
+    }
 
 	private initializeLight()
 	{
