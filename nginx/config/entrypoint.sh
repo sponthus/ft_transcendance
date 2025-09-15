3a240415f416ec8d86c1f32336f6ffff12698372
sponthus <sarah.ponthus@free.fr>
2025-09-15 19:23:19 +0200

    do not delete template files

--- nginx/config/entrypoint.sh
+++ nginx/config/entrypoint.sh
@@ -11,16 +11,11 @@
 #  C (country)
 # -nodes = no passphrase
 
-
 if env | grep -q "^NODE_ENV=development"; then
 	envsubst '${GAME_WS_PORT} ${SESSION_WS_PORT} ${API_PORT} ${DOMAIN_NAME}' < /etc/nginx/nginx.development.conf.template > /etc/nginx/nginx.conf
-	rm /etc/nginx/nginx.production.conf.template
 else
 	envsubst '${GAME_WS_PORT} ${SESSION_WS_PORT} ${API_PORT} ${DOMAIN_NAME}' < /etc/nginx/nginx.production.conf.template > /etc/nginx/nginx.conf
-	rm /etc/nginx/nginx.development.conf.template
 fi
-
-# tail -f
 
 mkdir -p /etc/nginx/sites-enabled
 
