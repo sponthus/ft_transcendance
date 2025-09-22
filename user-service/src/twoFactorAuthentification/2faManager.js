260b4065ac8c013573787a890d9465dcaa1d534f
Elodie Briere <ebriere@z3r6p4.42lyon.fr>
2025-09-22 22:41:50 +0200

    ds

---
+++ user-service/src/twoFactorAuthentification/2faManager.js
@@ -0,0 +1,7 @@
+import speakeasy from "speakeasy";
+
+//export async function   init
+
+//init 2fa 
+//validate 2fa
+//check  TOTP
---
+++ user-service/src/twoFactorAuthentification/use2fa.js
@@ -0,0 +1,17 @@
+/*import speakeasy from "speakeasy";
+
+export async function activate2Fa(request, reply)
+{
+    const   db = request.server.db;
+    const   idUser = request.server.idUser;
+
+    try
+    {
+        const secret = speakeasy.generateSecret();
+        
+    }
+    
+}
+
+//validate2Fa
+//--> passer 2fa_activated a 1;*/
