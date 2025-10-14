efd5d46eba1cbabc2751e8bb23435846433e44ee
Sarah Ponthus <sponthus@z1r5p5.42lyon.fr>
2025-10-14 17:01:34 +0200

    created_by in regular games

--- frontend/src/api/game-service/tournaments/newTournament.ts
+++ frontend/src/api/game-service/tournaments/newTournament.ts
@@ -60,12 +60,12 @@ export async function createTournament(name: string, playersList: Array<string>,
 			}
 		} else {
 			if (data.error)
-				throw new Error(data.error);
+				return { ok: false, error: data.error as string };
 			else
-				throw new Error("Unable to create tournament");
+				return { ok: false, error: "Unable to create tournament" };
 		}
 	}
-	catch(error) {
-		return {ok: false, error: error as string};
+	catch (error) {
+		return { ok: false, error: error as string };
 	}
 }
