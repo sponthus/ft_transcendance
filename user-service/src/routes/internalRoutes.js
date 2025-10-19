b509fa7d5e24b2c82daebd1f9415cbfc48e693f9
Elodie <briere.elodie@gmail.com>
2025-10-19 19:23:44 +0200

    fix

--- user-service/src/routes/internalRoutes.js
+++ user-service/src/routes/internalRoutes.js
@@ -1,9 +1,9 @@
 import { getBasicInfoOnUsers, getIdUserFromSlug, getUserInfosFromId } from "../internal-service/BasicInfoOnUsers.js";
-import { slugSchema } from "../tools/checkFormat.js";
+import { idUserSchema, slugSchema } from "../tools/checkFormat.js";
 
 export default async function internalRoutes(fastify)
 {
     fastify.get("/internal-service/users-info", { preHandler: [fastify.verifyApiKey] }, getBasicInfoOnUsers);
     fastify.get("/internal-service/:slug", { preHandler: [fastify.verifyApiKey], schema: { params: slugSchema } }, getIdUserFromSlug);
-	fastify.get("/internal-service/infos/:idUser", { preHandler: [fastify.verifyApiKey], schema: { params: slugSchema } }, getUserInfosFromId);
+	fastify.get("/internal-service/infos/:idUser", { preHandler: [fastify.verifyApiKey], schema: { params: idUserSchema } }, getUserInfosFromId);
 }
