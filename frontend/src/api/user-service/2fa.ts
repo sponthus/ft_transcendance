c83fc81042bdc3871f2df54562df690f4a340c3e
Elodie Briere <ebriere@z3r3p5.42lyon.fr>
2025-10-22 16:46:47 +0200

    schema avatar + delete token 2fa

--- frontend/src/api/user-service/2fa.ts
+++ frontend/src/api/user-service/2fa.ts
@@ -1,4 +1,4 @@
-type Success = {ok: true, qrCode?: string, status?: string, token?: string}
+type Success = {ok: true, qrCode?: string, status?: string, token?: string }
 type Failure = { ok: false; error: string };
 
 export type Result = Success | Failure 
@@ -43,6 +43,28 @@ export async function  checkTwoFaCode(code: string): Promise<Result>
         if (res.ok)
         {
             return ({ ok: true, status: data.status });
+        }
+        return ({ ok: false, error: data.message });
+    }
+    catch (err)
+    {
+        return ({ ok: false, error: "Network error" });
+    }
+}
+
+export async function  deleteCookieTwofa(): Promise<Result>
+{
+    try
+    {
+        const res = await fetch('/api/user/2fa/delete', 
+        {
+            method: 'PUT',
+            credentials: 'include',
+        });
+        const data = await res.json();
+        if (res.ok)
+        {
+            return ({ ok: true });
         }
         return ({ ok: false, error: data.message });
     }
