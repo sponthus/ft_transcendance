bd24e7f3c663b1170e875589be078cccd8c9a9f2
Sarah Ponthus <sponthus@z3r3p5.42lyon.fr>
2025-11-03 18:26:19 +0100

    Lock camera

--- frontend/src/babylon/displaying/renderScene.ts
+++ frontend/src/babylon/displaying/renderScene.ts
@@ -140,8 +140,8 @@ export class renderScene {
 		this._isocamera.orthoRight  =  halfWidth;
 		this._isocamera.orthoTop    =  halfHeight;
 		this._isocamera.orthoBottom = -halfHeight;
-		// this._isocamera.detachControl();
-		this._isocamera.attachControl();
+		this._isocamera.detachControl();
+		// this._isocamera.attachControl();
 	}
 
 	private  async _initLight() {
